67591af093de6ade1dfb5681adb8a19503966b54
alejandro <alejandro@debian-BULLSEYE-live-builder-AMD64>
2021-11-11 23:16:32 +0100

    Installed dependencies and bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { HomeComponent } from './home/home.component';
+import { NgbPaginationModule, NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    HomeComponent
   ],
   imports: [
+    NgbModule,
+    NgbPaginationModule,
     BrowserModule,
     AppRoutingModule
   ],
